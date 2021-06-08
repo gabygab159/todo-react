d9ffc488b3a301ccb9009b530aa8b94729e22dad
gabrieljchen <gabriel.jchen@gmail.com>
2021-06-08 11:03:56 -0400

    create handleSubmit and handleChange

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default  function Form(props) {
 
-function handleSubmit(event) {
-  event.preventDefault();
-  alert('Hello nerd')
-}
+  const [name, setName] = useState('')
+
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    props.addTask(name)
+    setName('')
+  }
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
     return (
         <form onSubmit={handleSubmit}>
         <h2 className="label-wrapper">
@@ -19,6 +27,8 @@ function handleSubmit(event) {
           className="input input__lg"
           name="text"
           autoComplete="off"
+          value={name}
+          onChange={handleChange}
         />
         <button type="submit" className="btn btn__primary btn__lg">
           Add
