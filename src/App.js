717678a46c4043edbeb3ef7f37e80f0ecb02fa56
gabrieljchen <gabriel.jchen@gmail.com>
2021-06-09 13:29:50 -0400

    add delete function

--- src/App.js
+++ src/App.js
@@ -24,7 +24,13 @@ function App(props) {
     setTasks(updatedTasks)
   }
 
-  const taskList = tasks.map(task => <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} toggleTaskCompleted={toggleTaskCompleted} />)
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id)
+    setTasks(remainingTasks)
+  
+  }
+
+  const taskList = tasks.map(task => <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} toggleTaskCompleted={toggleTaskCompleted} deleteTask={deleteTask} />)
 
 
 
